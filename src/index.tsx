491fe8ffa2f23a0f6397b0f39cef789c3f2ad6ed
Valery Zinchenko <minicablestone@gmail.com>
2025-02-26 17:37:01 +0300

    Add files via upload

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,16 @@
+import "./polyfills"
+import "./error-overlay"
 
+import { WebInflator } from "@denshya/proton"
+
+import App from "./App"
+import applyCustomAttributes from "./custom-attributes"
+
+
+const inflator = new WebInflator
+inflator.flags.debug = true
+applyCustomAttributes(inflator)
+
+
+const inflated = inflator.inflate(<App />)
+document.getElementById("root")!.replaceChildren(inflated)
