35f13e063e34722ceee52158017f37523dab665a
Valery Zinchenko <minicablestone@gmail.com>
2025-02-26 17:54:25 +0300

    Update polyfills.ts

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,7 +1,1 @@
-// Conditional ESM module loading (Node.js and browser)
-// @ts-ignore: Property 'UrlPattern' does not exist
-if (!globalThis.URLPattern) {
-  await import("urlpattern-polyfill")
-}
-
 export { }
