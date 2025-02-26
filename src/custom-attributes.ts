1d92bd18c0ba7c5fada389d3dfdd763c9493f5d0
Valery Zinchenko <minicablestone@gmail.com>
2025-02-26 17:57:51 +0300

    Update custom-attributes.ts

--- src/custom-attributes.ts
+++ src/custom-attributes.ts
@@ -1,24 +1,13 @@
-import { Flowable } from "@denshya/flow"
 import { WebInflator } from "@denshya/proton"
-
-import { bemFlow } from "./utils/bem"
-import { castArray } from "./utils/common"
 
 declare global {
   namespace JSX {
-    interface CustomAttributes {
-      /** Applies modifiers based `className` and overrides `className` attribute. */
-      classMods?: { [K in keyof never]: Flowable<unknown> } | Flowable<unknown>[]
-    }
+    interface CustomAttributes { }
   }
 }
 
 function applyCustomAttributes(inflator: WebInflator) {
-  inflator.jsxAttributes.set("classMods", context => {
-    if (context.value == null) return
-
-    context.bind("className", bemFlow(context.props.className as never, ...castArray(context.value)))
-  })
+  // inflator.jsxAttributes.set("special", context => { })
 }
 
 export default applyCustomAttributes
